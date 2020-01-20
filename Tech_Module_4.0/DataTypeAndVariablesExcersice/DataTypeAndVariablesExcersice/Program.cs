e45cbeba9be4763d211f1804fa95a8d63cad2233
ver4eto <50442741+ver4eto@users.noreply.github.com>
2020-01-20 19:35:12 +0200

    Data Types and Variables Exercise

---
+++ Tech_Module_4.0/DataTypeAndVariablesExcersice/DataTypeAndVariablesExcersice/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Numerics;
+using System.Threading.Tasks;
+
+namespace DataTypeAndVariablesExcersice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine();
+
+            int valueInt;
+            float valueFloat;
+            char valueChar;
+            bool vaalueBool;
+
+            while (input != "END")
+            {
+                if (int.TryParse(input,out valueInt))
+                {
+                    Console.WriteLine($"{input} is integer type");
+                }
+                else if(float.TryParse(input,out valueFloat))
+                {
+                    Console.WriteLine($"{input} is floating point type");
+                }
+                else if (char.TryParse(input,out valueChar))
+                {
+                    Console.WriteLine($"{input} is character type");
+                }
+                else if (bool.TryParse(input, out vaalueBool))
+                {
+                    Console.WriteLine($"{input} is boolean type");
+                }
+                else
+                {
+                    Console.WriteLine($"{input} is string type");
+                }
+                
+                input = Console.ReadLine();
+            }
+            
+
+        }
+    }
+}
