5de5f4a09f4f97ae9b8e9291de306c41876eec88
ver4eto <50442741+ver4eto@users.noreply.github.com>
2020-01-20 19:31:52 +0200

    Array Exercise

---
+++ Tech_Module_4.0/arraysExcercise/arraysExcercise/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+
+namespace arraysExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = int.Parse(Console.ReadLine());
+            int countOfNumbers = int.Parse(Console.ReadLine());
+
+            int[] myArray = new int[length];
+            int sum = 0;
+
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (i==0)
+                {
+                    myArray[i] = 1;
+                }
+                else
+                {
+                    for (int j = i; j <= countOfNumbers && j > 0; j--)
+                    {
+                        
+                        sum += myArray[j];
+
+                    }
+                    myArray[i] = sum;
+                }
+
+                sum = 0;
+                Console.Write(myArray[i]+" ");
+            }
+
+}
+    }
+}
+
+
+
+
---
+++ Tech_Module_4.0/arraysExcercise/ladyBugArrayExcercise.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq;
+
+
+namespace arraysExcercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            GradeInWOrds(number);
+
+        }
+
+        static void GradeInWOrds(double grade)
+        {
+            if (grade<=2.99)
+            {
+                Console.WriteLine("Fail");
+            }
+            else if (grade>2.99&&grade   <=3.49)
+            {
+                Console.WriteLine("Poor");
+            }
+            else if(grade>=3.50&&grade<=4.49)
+            {
+                Console.WriteLine("Good");
+            }
+            else if (grade>=4.50&&grade<=5.49)
+            {
+                Console.WriteLine("Very good");
+            }
+            else if (grade>=5.50)
+            {
+                Console.WriteLine("Excellent");
+            }
+        }
+    }
+}
+
+
+
+
