36ec6cc204f3a9d090ee1135b2eb08d8d6ab2196
ver4eto <50442741+ver4eto@users.noreply.github.com>
2020-01-21 13:57:10 +0200

    Solid Lab and exercises were added

---
+++ C# OOP/SolidLAB/SolidLAB/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SolidLAB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
