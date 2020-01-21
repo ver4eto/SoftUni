d9a00405d5e037f136dfb8bc9184da38cdc114bb
ver4eto <50442741+ver4eto@users.noreply.github.com>
2020-01-21 13:28:01 +0200

    Iterators and comparators Exercises added

---
+++ C#Advanced/Iterators And ComparatorsExcersice/Foggy/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Foggy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> stones = Console.ReadLine()
+                .Split(new[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            Lake lake = new Lake(stones);
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
