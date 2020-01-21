04a0b7aeff8b6a88010a90829e9fee04db5428a7
ver4eto <50442741+ver4eto@users.noreply.github.com>
2020-01-21 13:23:57 +0200

    C# exam solutions added

---
+++ C#Advanced/Exam23062019/CAdvancedExam/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CAdvancedExam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //            Glass   25
+            //Aluminium   50
+            //Lithium 75
+            //Carbon fiber    100
+
+
+            int glass = 25;
+            int glassCount = 0;
+            int aluminum = 50;
+            int aluminumCount = 0;
+
+            int lithium = 75;
+            int lithiumCount = 0;
+
+            int carbonFiber = 100;
+            int carbonCount = 0;
+
+            int[] inputChemicals = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] phisicalItemsInput = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> chemicalLiquids = new Queue<int>(inputChemicals);
+            Stack<int> phisicalItem = new Stack<int>(phisicalItemsInput);
+            List<string> allBuildProducts = new List<string>();
+
+            while (chemicalLiquids.Count>0 && phisicalItem.Count>0)
+            {
+                int currentChemical = chemicalLiquids.Peek();
+                int currentPhisical = phisicalItem.Peek();
+
+                int bothSum = currentChemical + currentPhisical;
+
+
+                if (bothSum==glass || bothSum==aluminum || bothSum==lithium || bothSum==carbonFiber)
+                {
+                    chemicalLiquids.Dequeue();
+                    phisicalItem.Pop();
+                    if (bothSum==glass)
+                    {
+                        glassCount++;
+                        allBuildProducts.Add("glass");
+                    }
+                    else if (bothSum==aluminum)
+                    {
+                        aluminumCount++;
+                        allBuildProducts.Add("aluminium");
+                    }
+                    else if (bothSum==lithium)
+                    {
+                        lithiumCount++;
+                        allBuildProducts.Add("lithium");
+                    }
+                    else if (bothSum==carbonFiber)
+                    {
+                        carbonCount++;
+                        allBuildProducts.Add("carbon");
+                    }
+                }
+                else
+                {
+                    chemicalLiquids.Dequeue();
+                    currentPhisical += 3;
+                    phisicalItem.Pop();
+                    phisicalItem.Push(currentPhisical);
+                }
+            }
+
+            if (allBuildProducts.Contains("glass") && allBuildProducts.Contains("aluminium")
+                && allBuildProducts.Contains("lithium")&& allBuildProducts.Contains("carbon"))
+            {
+                Console.WriteLine( "Wohoo! You succeeded in building the spaceship!");
+            }
+            else
+            {
+                Console.WriteLine("Ugh, what a pity! You didn't have enough materials to build the spaceship.");
+            }
+            if (chemicalLiquids.Count==0)
+            {
+                Console.WriteLine("Liquids left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Liquids left: {string.Join(", ",chemicalLiquids)}");
+            }
+            if (phisicalItem.Count==0)
+            {
+                Console.WriteLine("Physical items left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Physical items left: {string.Join(", ",phisicalItem)}");
+            }
+
+            //Dictionary<string, int> materials = new Dictionary<string, int>();
+            //materials.Add("");
+
+            Console.WriteLine($"Aluminium: {aluminumCount}");
+            Console.WriteLine($"Carbon fiber: {carbonCount}");
+            Console.WriteLine($"Glass: {glassCount}");
+            Console.WriteLine($"Lithium: {lithiumCount}");
+        }
+    }
+}
---
+++ C#Advanced/Exam23062019/Space Station Establishment/Program.cs
@@ -0,0 +1,235 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Space_Station_Establishment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[size, size];
+
+            int stephansRow = -1;
+            int stephansCol = -1;
+
+            //Dictionary<int, int> blackHoles = new Dictionary<int, int>();
+            List<int> blackHoles = new List<int>();
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                char[] input = Console.ReadLine().ToCharArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                    if (matrix[row, col]=='S')
+                    {
+                        stephansRow = row;
+                        stephansCol = col;
+                    }
+                    if (matrix[row, col]=='O')
+                    {
+                        //blackHoles.Add(row, col);
+                        blackHoles.Add(row);
+                        blackHoles.Add(col);
+                    }
+                }
+            }
+
+            bool isVoid = false;
+            int starPower = 0;
+
+            while (!isVoid/* && starPower<50*/)
+            {
+                string command = Console.ReadLine();
+                switch (command)
+                {
+                    case "up":
+                        int oldRow = stephansRow;
+                        stephansRow -= 1;
+                       
+                        if (IsInsideMatrix(matrix, stephansRow, stephansCol))
+                        {
+                            if (FoundStar(matrix, stephansRow, stephansCol))
+                            {
+                                int currentPower = int.Parse(matrix[stephansRow, stephansCol].ToString());
+                                starPower += currentPower;
+                                matrix[oldRow, stephansCol] = '-';
+                                matrix[stephansRow, stephansCol] = 'S';
+                            }
+                            else if (matrix[stephansRow, stephansCol]=='O')
+                            {
+                                matrix[stephansRow, stephansCol] = '-';
+                                stephansRow = blackHoles[2];
+                                stephansCol = blackHoles[3];
+                                matrix[stephansRow, stephansCol] = 'S';
+                               
+                            }
+                            else if (matrix[stephansRow, stephansCol]=='-')
+                            {
+                                matrix[oldRow, stephansCol] = '-';
+                                matrix[stephansRow, stephansCol] = 'S';
+                            }
+                        }
+                        else
+                        {
+                            isVoid = true;
+                            stephansRow += 1;
+                            matrix[stephansRow, stephansCol] = '-';
+                        }
+                        break;
+                    case "down":
+                        oldRow = stephansRow;
+                        stephansRow += 1;
+
+                        if (IsInsideMatrix(matrix, stephansRow, stephansCol))
+                        {
+                            if (FoundStar(matrix, stephansRow, stephansCol))
+                            {
+                                int currentPower = int.Parse(matrix[stephansRow, stephansCol].ToString());
+                                starPower += currentPower;
+                                matrix[oldRow, stephansCol] = '-';
+                                matrix[stephansRow, stephansCol] = 'S';
+                            }
+                            else if (matrix[stephansRow, stephansCol] == 'O')
+                            {
+                                matrix[stephansRow, stephansCol] = '-';
+                                stephansRow = blackHoles[2];
+                                stephansCol = blackHoles[3];
+                                matrix[stephansRow, stephansCol] = 'S';
+
+                            }
+                            else if (matrix[stephansRow, stephansCol] == '-')
+                            {
+                                matrix[oldRow, stephansCol] = '-';
+                                matrix[stephansRow, stephansCol] = 'S';
+                            }
+                        }
+                        else
+                        {
+                            isVoid = true;
+                            stephansRow -= 1;
+                            matrix[stephansRow, stephansCol] = '-';
+                        }
+                        break;
+                    case "left":
+                        int oldCol = stephansCol;
+                        stephansCol-= 1;
+
+                        if (IsInsideMatrix(matrix, stephansRow, stephansCol))
+                        {
+                            if (FoundStar(matrix, stephansRow, stephansCol))
+                            {
+                                int currentPower = int.Parse(matrix[stephansRow, stephansCol].ToString());
+                                starPower += currentPower;
+                                matrix[stephansRow, oldCol] = '-';
+                                matrix[stephansRow, stephansCol] = 'S';
+                            }
+                            else if (matrix[stephansRow, stephansCol] == 'O')
+                            {
+                                matrix[stephansRow, oldCol] = '-';
+                                stephansRow = blackHoles[2];
+                                stephansCol = blackHoles[3];
+                                matrix[stephansRow, stephansCol] = 'S';
+
+                            }
+                            else if (matrix[stephansRow, stephansCol] == '-')
+                            {
+                                matrix[stephansRow, oldCol] = '-';
+                                matrix[stephansRow, stephansCol] = 'S';
+                            }
+                        }
+                        else
+                        {
+                            isVoid = true;
+                            stephansCol += 1;
+                            matrix[stephansRow, stephansCol] = '-';
+                        }
+                        break;
+                    case "right":
+                        oldCol = stephansCol;
+                        stephansCol += 1;
+
+                        if (IsInsideMatrix(matrix, stephansRow, stephansCol))
+                        {
+                            if (FoundStar(matrix, stephansRow, stephansCol))
+                            {
+                                int currentPower = int.Parse(matrix[stephansRow, stephansCol].ToString());
+                                starPower += currentPower;
+                                matrix[stephansRow, oldCol] = '-';
+                                matrix[stephansRow, stephansCol] = 'S';
+                            }
+                            else if (matrix[stephansRow, stephansCol] == 'O')
+                            {
+                               
+                                matrix[stephansRow, oldCol] = '-';
+                                matrix[stephansRow, oldCol + 1] = '-';
+                                stephansRow = blackHoles[2];
+                                stephansCol = blackHoles[3];
+                                matrix[stephansRow, stephansCol] = 'S';
+
+                            }
+                            else if (matrix[stephansRow, stephansCol] == '-')
+                            {
+                                matrix[stephansRow, oldCol] = '-';
+                                matrix[stephansRow, stephansCol] = 'S';
+                            }
+                        }
+                        else
+                        {
+                            isVoid = true;
+                            stephansCol -= 1;
+                            matrix[stephansRow, stephansCol] = '-';
+                        }
+                        break;
+                    default:
+                        break;
+                }
+
+                if (starPower>=50)
+                {
+                    break;
+                }
+            }
+
+            if (isVoid)
+            {
+                Console.WriteLine("Bad news, the spaceship went to the void.");
+            }
+            else
+            {
+                Console.WriteLine("Good news! Stephen succeeded in collecting enough star power!");
+            }
+            Console.WriteLine($"Star power collected: {starPower}");
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        private static bool FoundStar(char[,] matrix, int newRow, int stephansCol)
+        {
+            char current = matrix[newRow, stephansCol];
+            if (Char.IsDigit(current))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private static bool IsInsideMatrix(char[,] matrix, int newRow, int stephansCol)
+        {
+            if (newRow>=0 && newRow<matrix.GetLength(0) && stephansCol>=0 && stephansCol<matrix.GetLength(1))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
