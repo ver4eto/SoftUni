1383a3a27869bd79f698901ffbbe92528eabd4d2
ver4eto <50442741+ver4eto@users.noreply.github.com>
2020-01-21 21:19:17 +0200

    Formatting content lines in response string

--- Basic_Web/HTTP/Program.cs
+++ Basic_Web/HTTP/Program.cs
@@ -23,8 +23,8 @@ namespace HTTP
                 using (NetworkStream networkStream = client.GetStream())
                 {
                     byte[] requestBytes = new byte[10000000];
-
                     //TODO: use buffer
+
                     int bytesRead = networkStream.Read(requestBytes, 0, requestBytes.Length);
                     string request = Encoding.UTF8.GetString(requestBytes, 0, bytesRead);
                     string responseText = @"<form action='/Account/Login' method='post'>
@@ -35,9 +35,9 @@ namespace HTTP
 </form>"; 
                     string respone = "HTTP/1.1 200 OK" + NewLine +
                         "Server: SoftUniServer/1.0" + NewLine +
-                        "Content - Type: text/html" + NewLine +                        
+                        "Content-Type: text/html" + NewLine +                        
                         "Content-Disposition: attachment; filename=vera.html" + NewLine+
-                        "Content - Lenght: " + responseText.Length + NewLine +
+                        "Content-Lenght: " + responseText.Length + NewLine +
                         NewLine +
                         responseText;
                     byte[] responesBytes = Encoding.UTF8.GetBytes(respone);
